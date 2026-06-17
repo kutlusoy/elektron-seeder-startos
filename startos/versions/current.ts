831ad7a6ffea9858e6c6ee4e515bc6d2636edde3
Ali Kutlusoy <ali@kutlusoy.com>
2026-06-17 08:51:39 +0200

    Update current.ts

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -1,7 +1,7 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
 export const current = VersionInfo.of({
-  version: '0.1.1:0',
+  version: '0.1.1:22',
   releaseNotes: 'Initial release.',
   migrations: {
     up: async () => {},
